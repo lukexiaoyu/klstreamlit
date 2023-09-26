c7856fddde8139b0edd5a1a0460e4cff4f8fae25
lukexiao <814008000@163.com>
2023-09-26 11:16:35 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+"""
+# My first app
+Here's our first attempt at using data to create a table:
+"""
+
+import streamlit as st
+import pandas as pd
+
+a=90
+b=60
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+st.write(a+b)
+st.write(a-b)
+st.write(df)
